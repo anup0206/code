f096f9f258359424246aa3a96f7a602de25fe444
anupam <alinedtwins@gmail.com>
2025-04-21 10:07:38 +0545

    new

--- python/Mega-project1-jarvis/main.py
+++ python/Mega-project1-jarvis/main.py
@@ -1,17 +1,25 @@
-#!/usr/bin/env python3
-import speech_recognition as sr
-import webbrowser
-import pyttsx3
+# #!/usr/bin/env python3
+# import speech_recognition as sr
+# import webbrowser
+# import pyttsx3
 
-recognizer = sr.Recognizer()
-engine  = pyttsx3.init()
-def speak(text):
-    engine.say(text)
-    engine.runAndWait()
+# recognizer = sr.Recognizer()
+# engine  = pyttsx3.init()
+# def speak(text):
+#     engine.say(text)
+#     engine.runAndWait()
 
 
-if __name__ =="__main__":
-    speak("Hey , myself pptsx3")
+# if __name__ =="__main__":
+#     speak("Hey , myself pptsx3")
     
 
+
+
+
+import pyttsx3
+
+engine  = pyttsx3.init()
+engine.say("Hey , myself pptsx3")
+engine.runAndWait()
 
